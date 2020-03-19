5de16d3d5e785af991dcf9fad4b6662c78ec6201
pdt-shoffman <35748040+pdt-shoffman@users.noreply.github.com>
2020-03-19 06:24:00 -0700

    set

--- signin.js
+++ signin.js
@@ -49,8 +49,8 @@ $('#inputPassword').keypress(function(e) {
     }
 });
 
-
-$('#signin-button').on('click', function() {
+//Exercise Pt2 - FIX ME!
+$('').on('', function() {
     if (email != $('#inputEmail').val()) count = 0;
     count++;
     email = $('#inputEmail').val();
